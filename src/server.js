81edd43c70ec6b707737f3ae5478b52f33302f82
Victor Setsuo <victor.setsuo@gmail.com>
2025-05-25 11:17:18 -0300

    Implementando CronJob

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const http = require("http");
+const { CronJob } = require("cron");
+const https = require('https');
 
 const routes = require("./routes");
 const { setupWebsocket } = require("./websocket");
@@ -16,6 +18,32 @@ app.use(express.json());
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+const job = CronJob.from({
+    cronTime: '*/15 * * * *',
+    onTick: function () {
+        console.log('This task runs every minute');
+        https.get('https://reticketify-aoj95.onrender.com/event', (resp) => {
+        let data = '';
+
+        // Um bloco de dados foi recebido.
+        resp.on('data', (chunk) => {
+          data += chunk;
+        });
+
+        // Toda a resposta foi recebida. Exibir o resultado.
+        resp.on('end', () => {
+          console.log(JSON.stringify(data));
+        });
+
+      }).on("error", (err) => {
+        console.log("Error: " + err.message);
+      });
+    },
+    start: true, // Start the job immediately
+});
+
